726f670b584606120b8b03cb2433f735ce82c887
n01028447 <n01028447@humber.ca>
2026-07-27 03:09:59 -0400

    Add Perishable class extending Grocery1

--- part3.js
+++ part3.js
@@ -31,3 +31,32 @@ console.log(`Discount price: $${Grocery1.applyDiscount(shampoo.price, 0.2)}`);
 
 
 
+class Perishalbe extends Grocery1 {
+
+    constructor(name, price, number, expirationDate) {
+        super(name, price, number);
+        this.expirationDate = expirationDate;
+    }
+
+    speak() {
+        console.log(`Product: ${this.name}, Expiration Date: ${this.expirationDate}, Price: $${this.price}, Quantity: ${this.number}, Total: $${this.getTotalVAlue()}`);
+    }
+}
+
+let tuna = new Perishalbe("Tuna", 5, 2, "Feb 01, 2027");
+let pasta = new Perishalbe("Pasta", 7, 3, "Oct 31, 2029");
+let lentil = new Perishalbe("Lentil", 6, 5, "Dec 12, 2028");
+
+tuna.speak();
+console.log(`Discount price: $${Perishalbe.applyDiscount(tuna.price, 0.5)}`);
+
+pasta.speak();
+console.log(`Discount price: $${Perishalbe.applyDiscount(pasta.price, 0.4)}`);
+
+lentil.speak();
+console.log(`Discount price: $${Perishalbe.applyDiscount(lentil.price, 0.3)}`);
+
+
+
+
+
